7c1692089281fdc5d1cd00f6743f5e7bcf35198a
BenHuyge <85291549+BenHuyge@users.noreply.github.com>
2021-06-08 12:29:21 +0200

    test

--- example.py
+++ example.py
@@ -1,14 +1,15 @@
 import pytest
-from pytest import approx
+
 
 
 temperature = 10
 
 def print_temperature():
   print(temperature)
+  
 def add(a, b):
     return a + b
 
 
 def test_add():
-    assert add(0.1, 0.2) = approx(0.3)
+    assert add(1, 2) = 3
